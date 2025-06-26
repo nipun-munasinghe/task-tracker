7728c9d0aab1212888b3537c5b7cd5d3053d22e7
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-06-26 23:50:35 +0530

    Add frequency select to AddTaskForm component
    Introduced a dropdown to select task frequency (daily or weekly) and connected the task name input to component state. Improved form layout using MUI components for better user experience.

--- src/components/AddTaskForm.tsx
+++ src/components/AddTaskForm.tsx
@@ -1,13 +1,32 @@
 import React, { useState } from 'react'
-import { Box, TextField } from '@mui/material';
+import { Box, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 
 const AddTaskForm = () => {
     const [name, setName] = useState("");
     const [frequency, setFrequency] = useState<"daily" | "weekly">("daily");
   return (
     <form>
-        <Box>
-            <TextField label='Task name'/>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+            <TextField 
+                label='Task name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder='Enter task name'
+                fullWidth
+            />
+            <FormControl fullWidth>
+                <InputLabel>Frequency</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={frequency}
+                    label="Frequency"
+                    onChange={(e) => setFrequency(e.target.value as "daily" | "weekly")}
+                >
+                    <MenuItem value="daily">Daily</MenuItem>
+                    <MenuItem value="daily">Weekly</MenuItem>
+                </Select>
+            </FormControl>
         </Box>
     </form>
   )
